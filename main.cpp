8020baceeb3d698f092ff9f907863b178c2fcdfa
GlowDanBios <46843591+GlowDanBios@users.noreply.github.com>
2019-06-24 16:43:22 +0300

    contest E

--- main.cpp
+++ main.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
-#include <String>
+#include <string>
+#include <stack>
 using namespace std;
 
-int main() {
-    string name;
-    cin >> name;
-    cout <<  "Hello, " << name <<"!";
-    return 0;
+int main(){
+    string command;
+    int n;
+    stack <int> steck;
+    while (true){
+        cin >> command;
+        if (command == "exit") {
+            cout << "bye" << endl;
+            break;
+        }
+        if (command == "push") {
+            cin >> n;
+            steck.push(n);
+            cout << "ok" << endl;
+        }
+        if (command == "pop") {
+            cout << steck.top() << endl;
+            steck.pop();
+        }
+        if (command == "back") {
+            cout << steck.top() << endl;
+        }
+        if (command == "size") {
+            cout << steck.size() << endl;
+        }
+        if (command == "clear") {
+            steck = stack<int>();
+            cout << "ok" << endl;
+        }
+    }
 }
